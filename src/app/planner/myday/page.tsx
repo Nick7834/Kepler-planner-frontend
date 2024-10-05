87479c4188aa2ff33516a9d1d22e381511a45f12
Nikita <history4527@gmail.com>
2024-10-05 11:36:54 +0300

    -v 2.4.27

--- src/app/planner/myday/page.tsx
+++ src/app/planner/myday/page.tsx
@@ -91,8 +91,6 @@ export default function Page() {
             <Loader />
         }
 
-        
-        
         </motion.div>
 
         <AddTask value={newTask} onChange={setNewTask} onAddTask={handleAddTaskToday} />
